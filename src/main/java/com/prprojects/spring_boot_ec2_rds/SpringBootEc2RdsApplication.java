e43585b5eb09978633d74608c24e0e7c150e1767
ppd10march <ppd10march@gmail.com>
2026-03-18 15:08:34 +0530

    New Spring Boot Project Commit

---
+++ src/main/java/com/prprojects/spring_boot_ec2_rds/SpringBootEc2RdsApplication.java
@@ -0,0 +1,13 @@
+package com.prprojects.spring_boot_ec2_rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEc2RdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2RdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prprojects/spring_boot_ec2_rds/SpringBootEc2RdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prprojects.spring_boot_ec2_rds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEc2RdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
